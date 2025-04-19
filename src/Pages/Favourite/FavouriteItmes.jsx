89033d9a76423a9e4f522636835463ba411010a6
JISHAN <jahedulislamdev@gmail.com>
2025-04-19 16:54:18 +0600

    added sort funtionality

--- src/Pages/Favourite/FavouriteItmes.jsx
+++ src/Pages/Favourite/FavouriteItmes.jsx
@@ -33,6 +33,24 @@ const FavouriteItmes = () => {
          setLoading(false)
       }, 700);
    }
+
+   // SORTING FAVOURITE PRODUCTS 
+   const [sortValue, setSortValue] = useState();
+   useEffect(() => {
+      if (!sortValue) return;
+      setLoading(true);
+      if (sortValue === "highToLow") {
+         setFaouriteItems(favouriteItems.sort((a, b) => b.price - a.price));
+      } else if (sortValue === "lowToHigh") {
+         setFaouriteItems(favouriteItems.sort((a, b) => a.price - b.price));
+      }
+      setTimeout(() => {
+         setLoading(false);
+      }, 300);
+      // return () => clearTimeout();
+   }, [favouriteItems, sortValue, setLoading])
+
+   // FALLBACK
    if (favouriteItems.length === 0) {
       return (
          <div className="h-96 flex flex-col justify-center items-center text-center">
@@ -50,11 +68,11 @@ const FavouriteItmes = () => {
       <div className='py-3 sm:px-7 px-1'>
          {/* sroting start */}
          <div className='flex justify-between items-center'>
-            <p className='uppercase font-medium text-xs @max-xs:font-semibold '>Favourite Products</p>
-            <select defaultValue="Pick a text editor" className="select select-sm w-32 P-0 focus:outline-0">
-               <option className='text-xs' disabled={true}>Pick a text editor</option>
-               <option className='text-xs'>Price High to Low</option>
-               <option className='text-xs'>Price Low to High</option>
+            <p className='uppercase font-medium text-xs md:text-lg'>Favourite Products</p>
+            <select onChange={(e) => setSortValue(e.target.value)} value={sortValue} className="select select-sm w-32 P-0 focus:outline-0">
+               <option value={''} disabled={true}>Pick a text editor</option>
+               <option value={'highToLow'}>Price High to Low</option>
+               <option value={'lowToHigh'}>Price Low to High</option>
             </select>
          </div>
          {/* sroting end */}
@@ -67,13 +85,14 @@ const FavouriteItmes = () => {
                            <div>
                               <img className='h-32 object-cover object-top border-e-3 border-red-500' src={i?.product_img} alt="" />
                            </div>
-                           <div>
+                           <div className='space-y-1'>
                               <p className='font-medium'>{i.title}</p>
-                              <p className=''>Category: {i.category}</p>
-                              <p className=''>Stock: {i.stock}</p>
+                              <p>Price : {i.price} BDT</p>
+                              <p>Category: {i.category}</p>
+                              <p>Stock: {i.stock}</p>
                            </div>
                         </Link>
-                        <button onClick={() => handleRemoveItem(i.model)} className=" p-0.5 rounded border-0 bg-red-600 text-white absolute top-1 right-1 cursor-pointer"><MdOutlineDelete className='size-5' /></button>
+                        <button onClick={() => handleRemoveItem(i.model)} className=" p-0.5 rounded border-0 bg-red-600 text-white absolute top-2 right-2 cursor-pointer"><MdOutlineDelete className='size-5' /></button>
                      </div>
                   ))
                }
